0e3ac6a744b88a4df6f9b6880d2637f75861a291
DannyEspinosaDiaz <66283165+DannyEspinosaDiaz@users.noreply.github.com>
2020-06-23 20:42:25 -0500

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<div class="clearfix"> </div>
-</div>
-<div class="copy">
-    <p>Copyright &copy; 2017 | Diseñado por <a href="https://www.facebook.com/fenixSasLaPlata/" target="_blank">Fénix S.A.S.</a></p>
-</div>
