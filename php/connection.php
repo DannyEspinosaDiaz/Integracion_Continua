1eb84b596f537f3e40942da18c904798438da26d
DannyEspinosaDiaz <66283165+DannyEspinosaDiaz@users.noreply.github.com>
2020-07-06 10:22:11 -0500

    Delete connection.php

--- php/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $mysqli = new mysqli('localhost', 'root', '', 'mybike');
-    if ($mysqli->connect_errno):
-        echo "Error al conectarse con MYSQL debido a un error ".$mysqli->connect_error;
-    endif;
-?>
